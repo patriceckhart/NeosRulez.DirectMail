55eb30391d5f3b3afefdb22b485bcdf1ec6d860a
Patric Eckhart <mail@patriceckhart.com>
2021-04-09 12:51:56 +0200

    + fix recipient list edit/show mode bug

--- Classes/Controller/RecipientListController.php
+++ Classes/Controller/RecipientListController.php
@@ -60,18 +60,29 @@ class RecipientListController extends ActionController
     }
 
     /**
+     * @param integer $offset
+     * @param integer $length
+     * @param integer $itemsPerLoad
      * @param \NeosRulez\DirectMail\Domain\Model\RecipientList $recipientList
      * @return void
      */
-    public function editAction($recipientList)
+    public function editAction(int $offset = 0, int $length = 50, int $itemsPerLoad = 50, $recipientList)
     {
-        $recipients = $this->recipientRepository->findByRecipientList($recipientList);
+        $recipients = $this->recipientRepository->findAll()->getQuery()->setOrderings(array('created' => \Neos\Flow\Persistence\QueryInterface::ORDER_DESCENDING))->execute();
+        $combinedRecipients = [];
         if($recipients) {
             foreach ($recipients as $recipient) {
                 $recipient->identifier = $this->persistenceManager->getIdentifierByObject($recipient);
+                $combinedRecipients[] = $recipient;
             }
+            $count = count($recipients);
+            $result = array_slice($combinedRecipients, $offset, $length);
+
+            $this->view->assign('offset', ($offset + $itemsPerLoad));
+            $this->view->assign('length', $itemsPerLoad);
+            $this->view->assign('count', $count);
         }
-        $this->view->assign('recipients', $recipients);
+        $this->view->assign('recipients', $result);
         $this->view->assign('recipientList', $recipientList);
     }
 
