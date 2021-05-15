901efcd9a5f3ba2244526cabf6dac84e5d87095b
Patric Eckhart <mail@patriceckhart.com>
2021-05-15 16:35:12 +0200

    email validate command action

--- Classes/Command/RecipientCommandController.php
+++ Classes/Command/RecipientCommandController.php
@@ -19,6 +19,12 @@ class RecipientCommandController extends CommandController {
      */
     protected $mergeService;
 
+    /**
+     * @Flow\Inject
+     * @var \NeosRulez\DirectMail\Domain\Repository\RecipientRepository
+     */
+    protected $recipientRepository;
+
 
     /**
      * Merging recipients
@@ -29,6 +35,32 @@ class RecipientCommandController extends CommandController {
         $this->outputLine("\n" .'Start merging duplicate recipients ...' . "\n");
         $this->mergeService->execute();
         $this->outputLine('The merging process is complete.'. "\n");
+    }
+
+    /**
+     * Validate recipient email address
+     *
+     * @return void
+     */
+    public function validateCommand() {
+        $this->outputLine("\n" .'Start validating email addresses ...' . "\n");
+        $recipients = $this->recipientRepository->findAll();
+        if($recipients) {
+            foreach ($recipients as $recipient) {
+                $email = $recipient->getEmail();
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    echo $email . " is invalid. I'm trying to fix it ...\n";
+                    $fixedEmail = str_replace(' ', '', $email);
+                    if (filter_var($fixedEmail, FILTER_VALIDATE_EMAIL)) {
+                        $recipient->setEmail($fixedEmail);
+                        $this->recipientRepository->update($recipient);
+                    } else {
+                        $this->recipientRepository->remove($recipient);
+                    }
+                }
+            }
+        }
+        $this->outputLine('The validating process is complete.'. "\n");
     }
 
 }
