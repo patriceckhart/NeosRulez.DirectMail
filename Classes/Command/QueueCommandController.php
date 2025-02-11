77629c4a0c47acfdcc33495c377b368486d7780c
patriceckhart <mail@patriceckhart.com>
2025-02-11 10:17:51 +0100

    preventMultipleJobExecution queue option added

--- Classes/Command/QueueCommandController.php
+++ Classes/Command/QueueCommandController.php
@@ -15,6 +15,14 @@ use NeosRulez\DirectMail\Domain\Service\DispatchService;
 class QueueCommandController extends CommandController
 {
 
+    const temporaryPathAndFileName = FLOW_PATH_TEMPORARY_BASE . '/directmail-queue';
+
+    /**
+     * @Flow\InjectConfiguration(package="NeosRulez.DirectMail", path="queue.preventMultipleJobExecution")
+     * @var bool
+     */
+    protected $preventMultipleJobExecution;
+
     /**
      * @Flow\Inject
      * @var DispatchService
@@ -28,10 +36,49 @@ class QueueCommandController extends CommandController
      */
     public function processCommand(): void
     {
-        $this->outputLine("\n" .'Start processing the queue ...' . "\n");
-        $result = $this->dispatchService->execute();
-        $this->outputLine($result);
-        $this->outputLine("\n" . 'The queue has been processed.'. "\n");
+        if($this->preventMultipleJobExecution) {
+            if(!$this->temporaryJobFileExist()) {
+                $this->handleTemporaryJobFile();
+                $this->outputLine("\n" .'Start processing the queue ...' . "\n");
+                $result = $this->dispatchService->execute();
+                $this->handleTemporaryJobFile(true);
+                $this->outputLine($result);
+                $this->outputLine("\n" . 'The queue has been processed.'. "\n");
+            } else {
+                $this->outputLine("\n" .'Queue is being processed ...' . "\n");
+            }
+        } else {
+            $this->outputLine("\n" .'Start processing the queue ...' . "\n");
+            $result = $this->dispatchService->execute();
+            $this->outputLine($result);
+            $this->outputLine("\n" . 'The queue has been processed.'. "\n");
+        }
+    }
+
+    /**
+     * @param bool $remove
+     * @return void
+     */
+    private function handleTemporaryJobFile(bool $remove = false): void
+    {
+        if(!$remove) {
+            file_put_contents(self::temporaryPathAndFileName, self::temporaryPathAndFileName);
+        } else {
+            if($this->temporaryJobFileExist()) {
+                unlink(self::temporaryPathAndFileName);
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    private function temporaryJobFileExist(): bool
+    {
+        if(file_exists(self::temporaryPathAndFileName)) {
+            return true;
+        }
+        return false;
     }
 
 }
