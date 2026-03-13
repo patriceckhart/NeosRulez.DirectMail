033b80f25cd7fefbb79b2b0b73faf270218f0a8a
Benjamin Klix <benjamin.klix@die-wegmeister.com>
2026-03-13 12:30:07 +0100

    feat: Allow additional css files through Settings

--- Classes/Domain/Service/CssToInlineService.php
+++ Classes/Domain/Service/CssToInlineService.php
@@ -12,12 +12,21 @@ use Pelago\Emogrifier\CssInliner;
 class CssToInlineService
 {
     /**
+     * @Flow\InjectConfiguration(package="NeosRulez.DirectMail", path="css")
+     * @var array
+     */
+    protected $cssSettings;
+
+    /**
      * @param string $html
      * @return string
      */
     public function execute(string $html): string
     {
         $css = file_get_contents(constant('FLOW_PATH_ROOT') . 'Web/_Resources/Static/Packages/NeosRulez.DirectMail/Styles/email.css');
+        foreach ($this->cssSettings['additionalFiles'] as $additionalFile) {
+            $css .= file_get_contents($additionalFile) ?: '';
+        }
         $visualHtml = CssInliner::fromHtml($html)->inlineCss($css)->renderBodyContent();
         return $visualHtml;
     }
